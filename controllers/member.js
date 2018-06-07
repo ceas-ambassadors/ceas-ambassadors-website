a8b762cc56ca0ae5b1a66d33d09a6b0143c4a968
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-06-06 21:45:29 -0400

    Add req checking to postLogin

--- controllers/member.js
+++ controllers/member.js
@@ -32,5 +32,20 @@ exports.getSignup = (req, res) => {
  * POST for the signup page
  */
 exports.postSignup = (req, res) => {
-  res.send('Message received.');
+  // TODO - clean up as necessary when #5 is closed
+  // TODO  - hopefully use express-validator to clean up checking for existance of variables
+  const alert = {};
+  alert.errorMessages = [];
+  if (req.body.email === '') {
+    alert.errorMessages.push('An email must be provided.');
+  }
+  if (req.body.email === '') {
+    alert.errorMessages.push('A password must be provided.');
+  }
+  if (alert.errorMessages.length > 0) {
+    // There was a validation error
+    // TODO - when #5 is closed this needs to redirect to getSignup()!
+    res.status(400).render('member/signup', { title: 'Sign up', alert: alert });
+  }
+  res.send(200);
 };
