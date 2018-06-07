865e7a5c9f061485b176d5573d122492dfbffa9c
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-06-06 21:24:10 -0400

    Create module for handing alerts between middleware
    This solution is incomplete. What I'd like to do is be able to handle alerts between redirects, but this solution doesn't accomplish this because req.locals doesn't get handed to the middleware on a redirect. One option for this is https://stackoverflow.com/a/38355597/7400944 or I could just use the connect-flash middleware for express, but that woudl mean giving up the customization I have on theflash system

--- app.js
+++ app.js
@@ -24,6 +24,54 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Database configuration
+ * The actual connecting to the database happend in `./models/index.js`,
+ * this file just syncs the models and verifies the connection is valid
+ */
+// Sync models - very important step
+models.sequelize.sync();
+// Check connection
+models.sequelize.authenticate().then(() => {
+  console.log('Connection to database established.');
+}).catch((err) => {
+  console.error('Unable to connect to the database:', err);
+  // Kill the process because there's no connection to the database
+  process.exit();
+});
+
+/**
+ * Alerts middleware
+ * This defines variables for alerting if they do not exist
+*/
+function alertMiddleware(req, res, next) {
+  // create req.locals object if it does not exist
+  // req.locals is the prefferred way of passing local variables between middleware
+  if (typeof req.locals === 'undefined') {
+    req.locals = {};
+  }
+  // if the alert object doesn't exist, create it
+  if (typeof req.locals.alert === 'undefined') {
+    req.locals.alert = {};
+  }
+  // If the messages arrays don't exist, create
+  if (typeof req.locals.alert.errorMessages === 'undefined' || !Array.isArray(req.locals.alert.errorMessages)) {
+    req.locals.alert.errorMessages = [];
+  }
+  if (typeof req.locals.alert.infoMessages === 'undefined' || !Array.isArray(req.locals.alert.infoMessages)) {
+    req.locals.alert.infoMessages = [];
+  }
+  if (typeof req.locals.alert.successMessages === 'undefined' || !Array.isArray(req.locals.alert.successMessages)) {
+    req.locals.alert.successMessages = [];
+  }
+  // continue execution to next middleware handler
+  next();
+}
+app.use(alertMiddleware);
+
+/**
+ * define routes
+ */
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
@@ -40,22 +88,6 @@ app.use((err, req, res/* , next */) => {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-/**
- * Database configuration
- * The actual connecting to the database happend in `./models/index.js`,
- * this file just syncs the models and verifies the connection is valid
- */
-// Sync models - very important step
-models.sequelize.sync();
-// Check connection
-models.sequelize.authenticate().then(() => {
-  console.log('Connection to database established.');
-}).catch((err) => {
-  console.error('Unable to connect to the database:', err);
-  // Kill the process because there's no connection to the database
-  process.exit();
 });
 
 module.exports = app;
