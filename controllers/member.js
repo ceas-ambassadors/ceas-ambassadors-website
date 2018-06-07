094dfd4eff5c56b6bf317b96260eff19656b1d36
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-06-06 22:58:53 -0400

    Add validation to /login

--- controllers/member.js
+++ controllers/member.js
@@ -1,7 +1,7 @@
 /**
  * This page is home to all code for controlling the member model
  */
-
+const { check, validationResult } = require('express-validator/check');
 // let models = require('../models/');
 /**
  * GET for the login page
@@ -31,21 +31,24 @@ exports.getSignup = (req, res) => {
 /**
  * POST for the signup page
  */
-exports.postSignup = (req, res) => {
-  // TODO - clean up as necessary when #5 is closed
-  // TODO  - hopefully use express-validator to clean up checking for existance of variables
-  const alert = {};
-  alert.errorMessages = [];
-  if (req.body.email === '') {
-    alert.errorMessages.push('An email must be provided.');
-  }
-  if (req.body.email === '') {
-    alert.errorMessages.push('A password must be provided.');
-  }
-  if (alert.errorMessages.length > 0) {
-    // There was a validation error
-    // TODO - when #5 is closed this needs to redirect to getSignup()!
-    res.status(400).render('member/signup', { title: 'Sign up', alert: alert });
-  }
-  res.send(200);
-};
+exports.postSignup = [
+  check('email').isEmail().withMessage('A valid email must be provided.'),
+  check('password').not().isEmpty().withMessage('A password must be provided.'),
+  check('confirmPassword').not().isEmpty().withMessage('A confirmation password must be provided.'),
+  (req, res) => {
+    const errors = validationResult(req).formatWith(({ msg }) => { return `${msg}`; });
+    if (!errors.isEmpty()) {
+      // There was a validation error
+      // TODO - when #5 is closed this needs to redirect to getSignup()!
+      res.status(400).render('member/signup', {
+        title: 'Sign up',
+        alert: {
+          errorMessages: errors.array(),
+        },
+      });
+    }
+
+
+    res.send(200);
+  },
+];
