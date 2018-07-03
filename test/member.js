229a8f8f7ef4b9811aca1cd7a9db28a665a2d81d
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-07-02 20:36:38 -0400

    Add framework for member tests

--- test/member.js
+++ test/member.js
@@ -4,12 +4,34 @@
 /* eslint-disable no-undef */
 // Immediately set environment to test
 process.env.NODE_ENV = 'test';
-// const request = require('supertest');
+const request = require('supertest');
 // const assert = require('assert');
-// const app = require('../app.js');
+const app = require('../app.js');
 // const models = require('../models');
-// const common = require('./common');
+const common = require('./common');
 
 describe('Member tests', () => {
+  after((done) => {
+    common.clearDatabase().then(() => {
+      done();
+    });
+  });
 
+  beforeEach((done) => {
+    common.clearDatabase().then(() => {
+      done();
+    });
+  });
+
+  describe('Member tests which require a logged in normal user', () => {
+    let agent = null;
+
+    beforeEach((done) => {
+      agent = request.agent(app);
+      // create and log in the user
+      common.createNormalUserSession(agent).then(() => {
+        done();
+      });
+    });
+  });
 });
