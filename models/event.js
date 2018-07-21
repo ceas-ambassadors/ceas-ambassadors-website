b445932a4c2de0d8fcf7e4fb7ec4c00b191d40e9
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-07-21 16:00:40 -0400

    Redo event hooks for previousDataValues
    Just like I misunderstood hooks for attendance records, I needed to make a
    change for events. Now, if an event changes it just calculate the difference
    between the two event lengths and updates members if necessary. I removed the
    function abnd switched to just writing the code in the hook itself

--- models/event.js
+++ models/event.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-use-before-define */
+/* eslint-disable no-underscore-dangle */ // Needed for sequelize
 /* eslint no-param-reassign: ["error", { "props": false }] */
 
 'use strict';
@@ -39,11 +40,59 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
     hooks: {
       // http://docs.sequelizejs.com/manual/tutorial/hooks.html
-      beforeUpdate: (event) => {
-        return handleEventUpdateOnAttendance(event, true);
+      beforeUpsert: () => {
+        throw Error('Upsert is hard to support for events.');
       },
       afterUpdate: (event) => {
-        return handleEventUpdateOnAttendance(event, false);
+        /**
+         * if the length of the event changes it needs to be reflected in member accounts who have
+         * been confirmed for attending it
+         * This code is relatively slow because of how much it could have to do, but because
+         * the server is asynchronous it should not matter
+         * This code is very clearly a bottleneck for the site if it got to the point where
+         * a lot of members were signing up for the same changed event (hundreds/thousands +)
+         */
+        // Meetings don't reflect time lengths so unimportant
+        if (event.meeting) {
+          // return promise for consistent return value
+          return Promise.resolve();
+        }
+
+        const lengthChange = (event.end_time - event.start_time)
+          - (event._previousDataValues.end_time - event._previousDataValues.start_time);
+        if (lengthChange === 0) {
+          // The time didn't change, so it is unnecessary to actually process all these members
+          // return promise for consistent return value
+          return Promise.resolve();
+        }
+
+        return sequelize.models.Attendance.findAll({
+          where: {
+            event_id: event.id,
+          },
+        }).then((attendances) => {
+          const promises = [];
+          for (let i = 0; i < attendances.length; i += 1) {
+            promises.push(sequelize.models.findById(attendances[i].member_email));
+          }
+          return Promise.all(promises).then((output) => {
+            const returns = []; // array of actions for return
+            // each attendance refers to a single member, so the two arrays are synced
+            for (let i = 0; i < output.length; i += 1) {
+              if (attendances[i].status === sequelize.models.Attendance.getStatusConfirmed()) {
+                returns.push(output[i].update({
+                  service: output[i].service + lengthChange,
+                }));
+              }
+              if (attendances[i].status === sequelize.models.Attendance.getStatusNotNeeded()) {
+                returns.push(output[i].update({
+                  service_not_needed: output[i].service_not_needed + lengthChange,
+                }));
+              }
+            }
+            return Promise.all(returns);
+          });
+        });
       },
       // beforeDestroy: - this is handled by cascading deletes and ensuring hooks are called
       // see options in Event.associate()
@@ -72,57 +121,5 @@ module.exports = (sequelize, DataTypes) => {
     models.Event.belongsTo(models.Member);
   };
 
-  /**
-   * if the length of the event changes it needs to be reflected in member accounts who have
-   * been confirmed for attending it
-   * This function handles both before update and after update - it always adds the length of the
-   * event, but the length is negative if it is before the update.
-   * This is done this way because it isn't possible to track the old values of start and end and
-   * compare them to the current values of start and end. The good thing is that this code is
-   * asynchronous and it therefore doesn't actually matter the order in which the before and end
-   * are executed if the before takes too long and regardless, the asynchronous nature will stop
-   * this from blocking a server to client response.
-   * @param {*} event - the event that has changed
-   * @param {*} deduct - should the length of the event be removed or added to the appropriate
-   *                     column
-   */
-  const handleEventUpdateOnAttendance = (event, deduct) => {
-    // Meetings don't reflect time lengths so unimportant
-    if (event.meeting) {
-      return Promise.resolve();
-    }
-
-    return sequelize.models.Attendance.findAll({
-      where: {
-        event_id: event.id,
-      },
-    }).then((attendances) => {
-      const promises = [];
-      for (let i = 0; i < attendances.length; i += 1) {
-        promises.push(sequelize.models.findById(attendances[i].member_email));
-      }
-      return Promise.all(promises).then((output) => {
-        const returns = []; // array of actions for return
-        let length = event.end_time - event.start_time;
-        if (deduct) {
-          length *= -1;
-        }
-        // each attendance refers to a single member, so the two arrays are synced
-        for (let i = 0; i < output.length; i += 1) {
-          if (attendances[i].status === sequelize.models.Attendance.getStatusConfirmed()) {
-            returns.push(output[i].update({
-              service: output[i].service + length,
-            }));
-          }
-          if (attendances[i].status === sequelize.models.Attendance.getStatusNotNeeded()) {
-            returns.push(output[i].update({
-              service_not_needed: output[i].service_not_needed + length,
-            }));
-          }
-        }
-        return Promise.all(returns);
-      });
-    });
-  };
   return Event;
 };
