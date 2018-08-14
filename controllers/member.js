4118a95f2234449a764ca2d61c378d72058325bd
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-08-14 16:48:22 -0400

    Add multer and form input

--- controllers/member.js
+++ controllers/member.js
@@ -3,7 +3,10 @@
  */
 const { check, validationResult } = require('express-validator/check');
 const passport = require('passport');
+const multer = require('multer');
 const models = require('../models/');
+
+const upload = multer({ dest: 'uploads/' });
 /**
  * GET for the login page
  */
@@ -292,53 +295,56 @@ exports.getSettings = getSettings;
  * @param {*} req - incoming request
  * @param {*} res - outgoing response
  */
-const postProfileUpdate = (req, res) => {
-  // Ensure there is a user signed in
-  if (!req.user) {
-    req.session.status = 401;
-    req.session.alert.errorMessages.push('You must be signed in to publish profile changes.');
-    return req.session.save(() => {
-      return res.redirect('/');
-    });
-  }
-  // Convert accend checkbox to bool
-  let accend = false;
-  if (req.body.accend === 'on') {
-    accend = true;
-  }
+const postProfileUpdate = [
+  upload.single('picture'),
+  (req, res) => {
+    console.log(req.file);
+    // Ensure there is a user signed in
+    if (!req.user) {
+      req.session.status = 401;
+      req.session.alert.errorMessages.push('You must be signed in to publish profile changes.');
+      return req.session.save(() => {
+        return res.redirect('/');
+      });
+    }
+    // Convert accend checkbox to bool
+    let accend = false;
+    if (req.body.accend === 'on') {
+      accend = true;
+    }
 
-  // Ensure that gradYear is a number
-  // correct syntax for let gradYear = req.body.gradYear;
-  let { gradYear } = req.body;
-  if (gradYear !== null && Number(gradYear) === 0) {
-    gradYear = null;
-  }
-  // Take any changes submitted as the whole truth - no verification needed.
-  return req.user.update({
-    first_name: req.body.firstName,
-    last_name: req.body.lastName,
-    major: req.body.major,
-    grad_year: gradYear,
-    clubs: req.body.clubs,
-    minors: req.body.minors,
-    accend, // shorthand for accend: accend,
-    hometown: req.body.hometown,
-    coops: req.body.coops,
-  }).then(() => {
-    req.session.status = 200;
-    req.session.alert.successMessages.push('Profile updated!');
-    return req.session.save(() => {
-      return res.redirect('/settings');
+    // Ensure that gradYear is a number
+    // correct syntax for let gradYear = req.body.gradYear;
+    let { gradYear } = req.body;
+    if (gradYear !== null && Number(gradYear) === 0) {
+      gradYear = null;
+    }
+    // Take any changes submitted as the whole truth - no verification needed.
+    return req.user.update({
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      major: req.body.major,
+      grad_year: gradYear,
+      clubs: req.body.clubs,
+      minors: req.body.minors,
+      accend, // shorthand for accend: accend,
+      hometown: req.body.hometown,
+      coops: req.body.coops,
+    }).then(() => {
+      req.session.status = 200;
+      req.session.alert.successMessages.push('Profile updated!');
+      return req.session.save(() => {
+        return res.redirect('/settings');
+      });
+    }).catch((err) => {
+      console.log(err);
+      req.session.alert.errorMessages.push('There was a problem. Please alert the tech chair if it continues.');
+      req.session.status = 500;
+      return req.session.save(() => {
+        return res.redirect('/settings');
+      });
     });
-  }).catch((err) => {
-    console.log(err);
-    req.session.alert.errorMessages.push('There was a problem. Please alert the tech chair if it continues.');
-    req.session.status = 500;
-    return req.session.save(() => {
-      return res.redirect('/settings');
-    });
-  });
-};
+  }];
 exports.postProfileUpdate = postProfileUpdate;
 
 /**
