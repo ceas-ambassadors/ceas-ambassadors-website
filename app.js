de38f87b7f5164ce3dfb432773323f158ba39eb1
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-08-17 23:27:44 -0400

    Don't log events to stdout in test environments

--- app.js
+++ app.js
@@ -31,8 +31,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-console.log(path.join(__dirname, 'public', 'images', 'favicon.ico'));
-app.use(logger('dev'));
+// Don't log page requests in tests
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
