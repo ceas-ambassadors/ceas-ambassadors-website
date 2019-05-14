6bb69b49e1a66606cab8251b50e74655585acd00
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-05-14 15:30:55 -0400

    Update session schema to have 36 char sids
    See #84 for reference

--- models/session.js
+++ models/session.js
@@ -1,6 +1,9 @@
 /**
  * Definition of session table - duplicated from https://github.com/mweibel/connect-session-sequelize/blob/master/lib/model.js
  * because it'll be useful to have an accessible table
+ * It's worth noting that this doesn't follow all the conventions that 
+ * the other tables follow because doing so would not work with the module that
+ * uses this table
  */
 
 'use strict';
@@ -8,7 +11,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Session = sequelize.define('Session', {
     sid: {
-      type: DataTypes.STRING(32),
+      type: DataTypes.STRING(36),
       primaryKey: true,
     },
     expires: DataTypes.DATE,
