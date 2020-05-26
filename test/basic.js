96ec1103a0685ea4348052852c6d0b824be51347
Liz Tremblay <tremblea@mail.uc.edu>
2020-05-26 16:43:40 -0400

    Add booklet page

--- test/basic.js
+++ test/basic.js
@@ -53,6 +53,13 @@ describe('Basic Tests', () => {
       .expect(200, done);
   });
 
+  // GET /booklet
+  it('GET /booklet', (done) => {
+    request.agent(app)
+      .get('/booklet')
+      .expect(200, done);
+  });
+
   // Test 404
   it('GET 404', (done) => {
     request.agent(app)
