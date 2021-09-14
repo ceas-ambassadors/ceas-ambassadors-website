642dd99847da2de711e6b8a8c6b6425529983ab7
Liz Tremblay <tremblea@mail.uc.edu>
2021-09-14 04:13:26 -0400

    single quotes fix

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      "script-src": ["'self'", "https://drive.google.com/", "https://docs.google.com/", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js", "https://code.jquery.com/jquery-3.3.1.slim.min.js"],
-      "script-src-elem": ["'self'", "https://code.jquery.com/jquery-3.3.1.slim.min.js", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"],
-      "frame-src": ["'self'", "https://www.google.com/", "https://drive.google.com/", "https://docs.google.com/", "https://calendar.google.com/", "https://mapsengine.google.com/"],
+      "script-src": ["'self'", 'https://drive.google.com/', 'https://docs.google.com/', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js', 'https://code.jquery.com/jquery-3.3.1.slim.min.js'],
+      "script-src-elem": ["'self'", 'https://code.jquery.com/jquery-3.3.1.slim.min.js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'],
+      "frame-src": ["'self'", 'https://www.google.com/', 'https://drive.google.com/', 'https://docs.google.com/', 'https://calendar.google.com/', 'https://mapsengine.google.com/'],
     },
   })
 );
